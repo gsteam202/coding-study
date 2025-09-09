54de65f176c4243137fd9ef640518b498c25e022
jihun202 <gsteam@naver.com>
2025-09-09 15:27:56 +0900

    1

--- CHAPTER18/42.ts
+++ CHAPTER18/42.ts
@@ -1,14 +1,18 @@
 function solve(rawInput: string): number {
   const input = rawInput.trim().split("\n");
-  const G = parseInt(input[0], 10);
-  const P = parseInt(input[1], 10);
-  const planes = input.slice(2, 2 + P).map(Number);
+  const G = parseInt(input[0], 10); // 탑승구 수
+  const P = parseInt(input[1], 10); // 비행기 수
+  const planes = input.slice(2, 2 + P).map(Number); // 도킹 가능 탑승구 정보
 
   const parent = new Array(G + 1).fill(0).map((_, i) => i);
+  console.log(`parent: ${parent}`)
 
-  function find(x: number): number {
+  function find(x: number): number { // 가장 가까운 사용 가능한 게이트 찾기
     if (parent[x] === x) return x;
-    return (parent[x] = find(parent[x]));
+    const root = find(parent[x]);
+    console.log(`root of ${root} / x: ${x}`);
+    parent[x] = root;
+    return root;
   }
 
   function union(a: number, b: number) {
@@ -21,19 +25,24 @@ function solve(rawInput: string): number {
 
   for (let g of planes) {
     let root = find(g);
-    if (root === 0) break;
+    console.log(`> ${g} / root: ${root}`);
+    if (root === 0) { // 0 = 게이트 없음
+      console.log(`> break: ${parent}`)
+      break;
+    }
     answer++;
     union(root, root - 1);
+    console.log(`> arrived: ${parent}`);
   }
 
   return answer;
 }
 
-console.log(solve(`4
-3
-4
-1
-1`));
+// console.log(solve(`4
+// 3
+// 4
+// 1
+// 1`));
 
 console.log(solve(`4
 6
